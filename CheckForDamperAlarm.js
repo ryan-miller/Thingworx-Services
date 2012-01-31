5d1b8c8576a9f064d4ec5cd944fccbdb19042875
Ryan Miller <miller2300@comcast.net>
2012-01-31 12:11:26 -0500

    added comments

--- CheckForDamperAlarm.js
+++ CheckForDamperAlarm.js
@@ -2,7 +2,8 @@ var glycolHigh = me.GlycolControlValve > 1 ? true : false,
    glycolLow = me.GlycolControlValve < 1 ? true : false,
    waterHigh = me.WaterControlValve > 1 ? true : false,
    waterLow = me.WaterControlValve < 1 ? true : false;
-   
+
+// one should near 0 while the other is open, else alarm it.
 if ((glycolLow && waterLow) || (glycolHigh && waterHigh)) {
    if (!me.InTCVAlarm) {
       me.InTCVAlarm = true;
