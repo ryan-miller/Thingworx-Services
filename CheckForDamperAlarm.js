478beef577cfef134001c836676ffd5fdd4cedb7
Ryan Miller <miller2300@comcast.net>
2012-01-31 12:10:01 -0500

    Check for Damper alarm patter

---
+++ CheckForDamperAlarm.js
@@ -0,0 +1,15 @@
+var glycolHigh = me.GlycolControlValve > 1 ? true : false,
+   glycolLow = me.GlycolControlValve < 1 ? true : false,
+   waterHigh = me.WaterControlValve > 1 ? true : false,
+   waterLow = me.WaterControlValve < 1 ? true : false;
+   
+if ((glycolLow && waterLow) || (glycolHigh && waterHigh)) {
+   if (!me.InTCVAlarm) {
+      me.InTCVAlarm = true;
+   }
+} else { 
+   if (me.InTCVAlarm) {
+      me.InTCVAlarm = false;
+   }
+}
+      
