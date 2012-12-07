4b8541d81c62661806321f10a8b06aa2a13fb539
Ryan Miller <miller2300@comcast.net>
2012-12-07 11:32:11 -0500

    github js conventions

--- BlogFiltering.js
+++ BlogFiltering.js
@@ -1,50 +1,41 @@
-var connection,
-    connections,
-    result,
-    getPostTags,
-    getFilterTag,
-    tags,
-    parent,
-    tag;
-    
-connections = Networks['ArizonaPower'].GetSubNetworkConnections({start:startNode});
-result = Resources['InfoTableFunctions'].CreateInfoTableFromDataShape({
-            dataShapeName : "PowerCompanyAssetsWithAlarms",
-            infoTableName : "AssetHierarchy"
-         });
+var connection, tag
+var connections = Networks['ArizonaPower'].GetSubNetworkConnections({ 
+  start: startNode 
+})
+var result = Resources['InfoTableFunctions'].CreateInfoTableFromDataShape({
+  dataShapeName: "PowerCompanyAssetsWithAlarms",
+  infoTableName: "AssetHierarchy"
+)}
 
-getPostTags = function(name) {
-         	         tags = "Assets:" + name + ";";
-         	         parent = Networks['ArizonaPower'].GetParentName({name:name});
-         	         return(parent == null)?"":tags+=getPostTags(parent);
-                   };
+var getPostTags = function(name) {
+  var tags = "Assets:" + name + ";"
+  var parent = Networks['ArizonaPower'].GetParentName({ name: name })
+  return (parent == null) ? "" : tags += getPostTags(parent)
+}
 
-getFilterTag = function(allTags) {
-         	      for each (var tag in allTags.rows) {
-         		      if (tag.vocabulary == "Assets")
-         			      return tag.vocabulary + ":"+ tag.vocabularyTerm;
-         	      }
-
-         	      return "";
-               };
+var tag
+var getFilterTag = function(allTags) {
+  for each (tag in allTags.rows) {
+    (tag.vocabulary == "Assets") ? return tag.vocabulary + ":" + tag.vocabularyTerm : ""
+  }
+}
 
 for each(connection in connections.rows) {
-
 	if (connection.to.substring(0,4) != "Cell") {
+    result.AddRow({
+		  to:             connection.to,
+      from:           connection.from,
+      connectionType: connection.connectionType,
 
-		result.AddRow({
-		   homeMashup : Things[connection.to].homeMashup,
-		   to : connection.to,
-		   toDescription : Things[connection.to].description,
-		   from : connection.from,
-		   connectionType : connection.connectionType,
-		   location : Things[connection.to].Location,
-		   assetType : Things[connection.to].ResourceType,
-		   AlarmState : Things[connection.to].AlarmState,
-		   inAlarm : Things[connection.to].InAlarm ? "alarm" : "noalarm",
-		   filterTags : getFilterTag(Things[connection.to].GetTagsAsInfoTable()),
-		   postTags : getPostTags(connection.to)
-		});
+      postTags:       getPostTags(connection.to),
+      filterTags:     getFilterTag(Things[connection.to].GetTagsAsInfoTable()),
 
+      inAlarm:        Things[connection.to].InAlarm ? "alarm" : "noalarm",
+      location:       Things[connection.to].Location,
+      assetType:      Things[connection.to].ResourceType,
+      AlarmState:     Things[connection.to].AlarmState,
+      homeMashup:     Things[connection.to].homeMashup,
+		  toDescription:  Things[connection.to].description
+    })
 	}
 }
