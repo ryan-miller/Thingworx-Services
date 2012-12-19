a1c1b932b45375abf166e085335aba014f161215
Ryan Miller <miller2300@comcast.net>
2012-12-19 13:56:08 -0500

    total rework of map display logic

--- ATS_MapManager_QueryMap.js
+++ ATS_MapManager_QueryMap.js
@@ -20,111 +20,195 @@ IconState:
 
 */
 
-//This service will list all of the Map data
 try {
-  
-  var singleDayInMs = 24 * 60 * 60 * 1000
-  var now = new Date()
-  var yesterdayInMs = now.getTime() - singleDayInMs
-  
-  // set up result data
+  var details = Resources['InfoTableFunctions'].CreateInfoTableFromDataShape({
+    infoTableName : "InfoTable",
+    dataShapeName : "ATSMapShape"
+  })
+
   var result = Resources['InfoTableFunctions'].CreateInfoTableFromDataShape({
     infoTableName : "InfoTable",
     dataShapeName : "ATSMapShape"
   })
 
-  // add location info rows
   var addLocationRows = (function () {
     var site
-
     for each (site in me.GetAccountSites().rows) {
-      result.AddRow({
-        Name:     site.name, 
-        NickName: site.SiteName, 
-        Location: site.Location,
-        State:    'Location'
+      details.AddRow({
+        Thing:          site.name, 
+        DisplayName:    site.SiteName, 
+        Location:       site.Location,
+        State:          'Location',
+        TypePriority:   1,
+        StatePriority:  999
       })
     }
   })()
 
-  // add device info rows
   var addDeviceRows = (function () {
-    var device
-    var recentAlert = false
-    var recentLogin = false
+    var device, state, priority
+    var recentAlert, recentLogin
     var loginDiffInSec
-    var IsConflicted, IsMobile, IsFixed
-    var State, MashupName
+    var singleDayInMs = 24 * 60 * 60 * 1000
+    var now = new Date()
+    var yesterdayInMs = now.getTime() - singleDayInMs
 
     for each(device in me.GetAccountDevices().rows){
-    
+      priority = 0
+      state = 'Device'
+
       recentAlert = (device.Alerts_LastAlertTime.getTime() > yesterdayInMs) ? true : false  
     
-      // recent Login occurs if lastLoginTime is between now and web_servercontactfrequency ago 
       loginDiffInSec = (now.getTime() - device.LastLogin.getTime()) / (60 * 1000)
       recentLogin = (loginDiffInSec < device.WEB_ServerContactFrequency) ? true : false
 
-      IsConflicted = device.IsConflictedLocation ? true : false
-
-      // devices may not have sites
-      if (device.Site === "") 
-        IsMobile = false
-      else 
-        IsMobile = Things[device.Site].IsMobile ? true : false
-
-      IsFixed = !(IsConflicted || IsMobile)
-
-      // build state !order is important
-      State = (IsFixed)       ? "Fixed" : 
-              (IsMobile)      ? "Mobile" : 
-              (IsConflicted)  ? "Conflicted" : ""   
-      if (recentLogin) {
-        State += "Recent"
+      if (device.IsConflictedLocation) {
+        state += 'Conflict'
+        priority = 1
+      } else if (recentAlert) {
+        state += 'Alert'
+        priority = 2
+      } else if (!recentLogin) {
+        state += 'Missing'
+        priority = 3
+      } else {
+        state += 'Normal'
+        priority = 4
       }
-      State += device.BaseSignType
 
-      // build mashup !order is important
-      MashupName = "ATSMap"
-      MashupName += (IsFixed) ? "Fixed" : ""
-      MashupName += (IsMobile) ? "Mobile" : ""
-      MashupName += (IsConflicted) ? "Conflicted" : ""
-      switch (device.BaseSignType) {
-        case "S":
-          MashupName += "Speed"
-          break
-        case "M":
-          MashupName += "Message"
-          break
-        default:
-      }
-      MashupName += "Device"
-          
-      result.AddRow({   
-        Name:         device.name,
-        Tags:         device.tags, 
-        NickName:     device.Nickname, 
+      details.AddRow({   
+        Thing:        device.name,
+        DisplayName:  device.Nickname, 
         Location:     device.Location,
-        IsConflicted: device.IsConflictedLocation,
-        
-        IsLocation: false,
-        IsLocation: false,
-        
-        State:        State,
-        IsFixed:      IsFixed,
-        IsMobile:     IsMobile,
-        MashupName:   MashupName,
-        RecentAlert:  recentAlert,
-        RecentLogin:  recentLogin
+        State:        state,
+        TypePriority: 2,
+        StatePriority: priority
       })
     }
   })()
-  
-  // filter based on service input query object
-  result = Resources['InfoTableFunctions'].Query({
-    t: result,
-    query: Query
+
+  var agg = Resources['InfoTableFunctions'].Aggregate({
+    columns : 'Location,TypePriority,StatePriority',
+    groupByColumns : 'Location',
+    t : details,
+    aggregates : 'COUNT,MIN,MIN'
   })
 
+  var findByTypeAndState = function (location, type, state) {
+    return Resources['InfoTableFunctions'].Query({
+      query: {
+        filters: {
+          type : 'AND',
+          filters : [{
+            type: 'GE',
+              fieldName: 'TypePriority',
+              value: type
+          }, {
+            type: 'EQ',
+            fieldName: 'StatePriority',
+            value: state
+          }, {
+            type: 'EQ',
+            fieldName: 'Location',
+            value: location
+          }]
+        }
+      },
+      t : details
+    })
+  }
+
+  var findByTypeAndLocation = function (t, type, loc) {
+    logger.warn('findbytypeandlocation' + t + type + loc)
+    return Resources['InfoTableFunctions'].Query({
+      query: {
+        filters: {
+          type : 'AND',
+          filters : [{
+            type: 'EQ',
+              fieldName: 'TypePriority',
+              value: type
+          }, {
+            type: 'EQ',
+            fieldName: 'Location',
+            value: loc
+          }]
+        }
+      },
+      t: t
+    })
+  }
+
+  var findByStateAndLocation = function (t, state, loc) {
+    logger.warn('findbystateandlocation' + t + state + loc)
+    return Resources['InfoTableFunctions'].Query({
+      query: {
+        filters: {
+          type : 'AND',
+          filters : [{
+            type: 'EQ',
+              fieldName: 'StatePriority',
+              value: state
+          }, {
+            type: 'EQ',
+            fieldName: 'Location',
+            value: loc
+          }]
+        }
+      },
+      t: t
+    })
+  }
+
+  var findState = function (loc) {
+    var allAtLocation = Resources['InfoTableFunctions'].Query({
+      query: {
+        filters : {
+          type: 'EQ',
+          fieldName: 'Location',
+          value: loc
+        }
+      },
+      t: details
+    })
+
+    var isSite      = (findByTypeAndLocation(allAtLocation, 1, loc).getRowCount() > 0)
+    var isConflict  = (findByStateAndLocation(allAtLocation, 1, loc).getRowCount() > 0)
+    var isAlert     = (findByStateAndLocation(allAtLocation, 2, loc).getRowCount() > 0)
+    var isMissing   = (findByStateAndLocation(allAtLocation, 3, loc).getRowCount() > 0)
+    var isNormal    = (findByStateAndLocation(allAtLocation, 4, loc).getRowCount() > 0)
+
+    var state
+
+    if (isSite) {
+      state = 'Location'
+      if (isConflict) 
+        state += 'Conflict'
+      state += (isAlert) ? 'Alert' : (isMissing) ? 'Missing' : (isNormal) ? 'Normal' : 'Empty'
+    } else {
+      state = 'Device'
+      state += (isConflict) ? 'Conflict' : (isAlert) ? 'Alert' : (isMissing) ? 'Missing' : 'Normal'
+    }
+    return state
+
+  } 
+
+  var row
+  var thing
+  var searchPriority
+
+  for each(row in agg.rows) {
+    searchPriority = (row.MIN_TypePriority === 1) ? 999 : row.MIN_StatePriority
+    thing = findByTypeAndState(row.Location, row.MIN_TypePriority, searchPriority)
+        
+    result.AddRow({
+      Thing:        thing.Thing,
+      DisplayName:  thing.DisplayName,
+      Location:     row.Location,
+      State:        findState(row.Location)
+    })
+
+  }
 } catch (err) {
   logger.error("Error getting Map: " + err)
 }
