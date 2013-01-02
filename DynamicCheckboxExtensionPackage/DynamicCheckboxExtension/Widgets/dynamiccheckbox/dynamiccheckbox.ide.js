b5a6f0c8b9956a4810de8b318b30f2007ad9bd74
Ryan Miller <miller2300@comcast.net>
2013-01-02 14:03:07 -0500

    style convention mods

--- DynamicCheckboxExtensionPackage/DynamicCheckboxExtension/Widgets/dynamiccheckbox/dynamiccheckbox.ide.js
+++ DynamicCheckboxExtensionPackage/DynamicCheckboxExtension/Widgets/dynamiccheckbox/dynamiccheckbox.ide.js
@@ -1,111 +1,108 @@
 ﻿TW.IDE.Widgets.dynamiccheckbox = function() {
 
-   this.widgetProperties = function() {
-      return {
-         'name': 'Dynamic Checkbox',
-         'description': 'Adds to checkbox with optional bindable value',
-         'category': ['Common'],
-         'dataSourceProperty': 'State',
-         'defaultBindingTargetProperty': 'State',
-         'supportsResetInputToDefault': true,
-         'properties': {
-            'Prompt': {
-               'defaultValue': 'Check this box',
-               'baseType': 'STRING'
-            },
-            'Width': {
-               'defaultValue': 120
-            },
-            'Height': {
-               'defaultValue': 20,
-               'isEditable': false
-            },
-            'State': {
-               'isBindingSource': true,
-               'isBindingTarget': true,
-               'baseType': 'BOOLEAN',
-               'defaultValue': false
-            },
-            'Disabled': {
-               'isBindingSource': true,
-               'isBindingTarget': true,
-               'baseType': 'BOOLEAN',
-               'defaultValue': false
-            },
-            'Visible': {
-               'isBindingSource': true,
-               'isBindingTarget': true,
-               'baseType': 'BOOLEAN',
-               'defaultValue': true
-            },
-            'Value': {
-               'isBindingTarget': true,
-               'isBindingSource': true,
-               'baseType': 'STRING',
-               'defaultValue': ''
-            },
-            'TabSequence': {
-               'description': 'Tab sequence index',
-               'baseType': 'NUMBER',
-               'defaultValue': 0
-            },
-            'Style': {
-               'baseType': 'STYLEDEFINITION',
-               'defaultValue': 'DefaultCheckboxStyle'
-            }
-         }
-      };
-   };
-
-   this.widgetEvents = function() {
-      return {
-         'Changed': {}
+  this.widgetProperties = function() {
+    return {
+      'name': 'Dynamic Checkbox',
+      'description': 'Adds to checkbox with optional bindable value',
+      'category': ['Common'],
+      'dataSourceProperty': 'State',
+      'defaultBindingTargetProperty': 'State',
+      'supportsResetInputToDefault': true,
+      'properties': {
+      'Prompt': {
+        'defaultValue': 'Check this box',
+        'baseType': 'STRING'
+      },
+      'Width': {
+        'defaultValue': 120
+      },
+      'Height': {
+        'defaultValue': 20,
+        'isEditable': false
+      },
+      'State': {
+        'isBindingSource': true,
+        'isBindingTarget': true,
+        'baseType': 'BOOLEAN',
+        'defaultValue': false
+      },
+      'Disabled': {
+        'isBindingSource': true,
+        'isBindingTarget': true,
+        'baseType': 'BOOLEAN',
+        'defaultValue': false
+      },
+      'Visible': {
+        'isBindingSource': true,
+        'isBindingTarget': true,
+        'baseType': 'BOOLEAN',
+        'defaultValue': true
+      },
+      'Value': {
+        'isBindingTarget': true,
+        'isBindingSource': true,
+        'baseType': 'STRING',
+        'defaultValue': ''
+      },
+      'TabSequence': {
+        'description': 'Tab sequence index',
+        'baseType': 'NUMBER',
+        'defaultValue': 0
+      },
+      'Style': {
+        'baseType': 'STYLEDEFINITION',
+        'defaultValue': 'DefaultCheckboxStyle'
       }
-   };
+    }
+  }
 
-   this.renderHtml = function() {
-      var html, 
-         textSize = TW.getTextSizeFromStyleDefinition(this.getProperty('Style')),
-         cssInfo = TW.getStyleCssTextualFromStyleDefinition(this.getProperty('Style')),
-         prompt = (this.getProperty('Prompt') !== undefined ? Encoder.htmlEncode(this.getProperty('Prompt')) : '');
+  this.widgetEvents = function() {
+    return {
+      'Changed': {}
+    }
+  }
 
-      if (cssInfo.length > 0) {
-         cssInfo = 'style="' + cssInfo + '"';
-      }
+  this.renderHtml = function() {
+    var html 
+    var textSize = TW.getTextSizeFromStyleDefinition(this.getProperty('Style')),
+    var cssInfo = TW.getStyleCssTextualFromStyleDefinition(this.getProperty('Style')),
+    var prompt = (this.getProperty('Prompt') !== undefined ? Encoder.htmlEncode(this.getProperty('Prompt')) : '')
 
-      html = '<div class="widget-content widget-checkbox ' + textSize + '" width="100%" height="100%"' + cssInfo + '>';
-      html += '<input type="checkbox" class="input-checkbox" />';
-      html += '<span class="prompt-checkbox">' + prompt + '</span>';
-      html += '</div>';
+    if (cssInfo.length > 0) {
+     cssInfo = 'style="' + cssInfo + '"'
+    }
 
-      return html;
-   };
+    html = '<div class="widget-content widget-checkbox ' + textSize + '" width="100%" height="100%"' + cssInfo + '>'
+    html += '<input type="checkbox" class="input-checkbox" />'
+    html += '<span class="prompt-checkbox">' + prompt + '</span>'
+    html += '</div>'
 
+    return html
+  }
 
-   this.afterSetProperty = function(name, value) {
-      var result = false;
-      switch (name) {
+  this.afterSetProperty = function(name, value) {
+    var result = false
+    switch (name) {
       case 'Value':
       case 'Visible':
       case 'Disabled':
       case 'Prompt':
       case 'Style':
-         result = true;
-         break;
-      }
-      return result;
-   };
+        result = true
+      break
+    }
+    return result
+  }
 
-   this.validate = function() {
-      var result = [];
-      if (!this.isPropertyBoundAsSource('State') && !this.isPropertyBoundAsTarget('State')) {
-         result.push({
-            severity: 'warning',
-            message: 'State for {target-id} is not bound as a source or to any target'
-         });
-      }
+  this.validate = function() {
+    var result = []
+    if (!this.isPropertyBoundAsSource('State') && !this.isPropertyBoundAsTarget('State')) {
+      result.push({
+        severity: 'warning',
+        message: 'State for {target-id} is not bound as a source or to any target'
+      })
+    }
 
-      return result;
-   };
-
-};
+    return result
+  }
+}
--- DynamicCheckboxExtensionPackage/DynamicCheckboxExtension/Widgets/dynamiccheckbox/dynamiccheckbox.runtime.js
+++ DynamicCheckboxExtensionPackage/DynamicCheckboxExtension/Widgets/dynamiccheckbox/dynamiccheckbox.runtime.js
@@ -1,168 +1,161 @@
 ﻿TW.Runtime.Widgets.dynamiccheckbox = function() {
+  var thisWidget = this
+  var defaultState = undefined
+  var defaultVisible = undefined
+  var defaultDisabled = undefined
+  var defaultValue = undefined
 
-   var thisWidget = this,
-       defaultState = undefined,
-       defaultVisible = undefined,
-       defaultDisabled = undefined,
-       defaultValue = undefined;
+  this.runtimeProperties = function() {
+    return {
+      'needsDataLoadingAndError': false
+    }
+  }
 
-   this.runtimeProperties = function() {
-      return {
-         'needsDataLoadingAndError': false
-      };
-   };
+  this.renderHtml = function() {
 
-   this.renderHtml = function() {
+    var html
+    var cssInfo = TW.getStyleCssTextualFromStyleDefinition(this.getProperty('Style', 'DefaultCheckboxStyle'))
+    var textSize = TW.getTextSizeFromStyleDefinition(this.getProperty('Style'))
+    var cssInfo = TW.getStyleCssTextualFromStyleDefinition(this.getProperty('Style'))
+    var prompt = (this.getProperty('Prompt') !== undefined ? Encoder.htmlEncode(this.getProperty('Prompt')) : '')
+    var disabled = (thisWidget.getProperty('Disabled') === true) ? 'disabled' : ''
+    var tabSequence = thisWidget.getProperty('TabSequence')
 
-      var html, 
-         cssInfo = TW.getStyleCssTextualFromStyleDefinition(this.getProperty('Style', 'DefaultCheckboxStyle')),
-         textSize = TW.getTextSizeFromStyleDefinition(this.getProperty('Style')),
-         cssInfo = TW.getStyleCssTextualFromStyleDefinition(this.getProperty('Style')),
-         prompt = (this.getProperty('Prompt') !== undefined ? Encoder.htmlEncode(this.getProperty('Prompt')) : ''),
-         disabled = (thisWidget.getProperty('Disabled') === true) ? 'disabled' : '',
-         tabSequence = thisWidget.getProperty('TabSequence');
+    defaultState = (String(thisWidget.getProperty('State')) == "true") ? true : false
+    defaultVisible = (String(thisWidget.getProperty('Visible')) == "true") ? true : false
+    defaultDisabled = (String(thisWidget.getProperty('Disabled')) == "true") ? true : false
+    defaultValue = thisWidget.getProperty('Value')
 
-      defaultState = (String(thisWidget.getProperty('State')) == "true") ? true : false;
-      defaultVisible = (String(thisWidget.getProperty('Visible')) == "true") ? true : false;
-      defaultDisabled = (String(thisWidget.getProperty('Disabled')) == "true") ? true : false;
-      defaultValue = thisWidget.getProperty('Value');
+    if (cssInfo.length > 0) {
+      cssInfo = 'style="' + cssInfo + '"'
+    }
 
-      if (cssInfo.length > 0) {
-         cssInfo = 'style="' + cssInfo + '"';
-      }
+    html = '<div class="widget-content widget-checkbox ' + textSize + '" width="100%" height="100%" ' + cssInfo + '>'
+    html += '<input type="checkbox" class="input-checkbox" ' + disabled + ' tabindex="' + tabSequence + '"></input>'
+    html += '<span class="prompt-checkbox">' + prompt + '</span>'
+    html += '</div>'
 
-      html = '<div class="widget-content widget-checkbox ' + textSize + '" width="100%" height="100%" ' + cssInfo + '>';
-      html += '<input type="checkbox" class="input-checkbox" ' + disabled + ' tabindex="' + tabSequence + '"></input>';
-      html += '<span class="prompt-checkbox">' + prompt + '</span>';
-      html += '</div>';
+    return html
+  }
 
-      return html;
-   };
+  this.afterRender = function() {
+    var widgetElement = this.jqElement
+    var checkboxElement = widgetElement.find('input')
+    var widgetProperties = this.properties
+    var widgetReference = this
 
-   this.afterRender = function() {
-      var widgetElement = this.jqElement,
-          checkboxElement = widgetElement.find('input'),
-          widgetProperties = this.properties,
-          widgetReference = this;
+    checkboxElement.bind('change', function() {
+      var newValue = checkboxElement.attr('checked')
 
-      checkboxElement.bind('change', function() {
-         var newValue = checkboxElement.attr('checked');
-         if (newValue === 'checked' || newValue === 'true') {
-            widgetReference.setProperty('State', true);
-         } else {
-            widgetReference.setProperty('State', false);
-         }
+      (newValue === 'checked' || newValue === 'true') ? widgetReference.setProperty('State', true) : widgetReference.setProperty('State', false)
 
-         widgetElement.triggerHandler('Changed');
-      });
+      widgetElement.triggerHandler('Changed')
+    })
 
-      var curState = widgetReference.getProperty('State');
-      if (String(curState) == "true") {
-         checkboxElement.prop('checked', true);
-         widgetReference.setProperty('State', true);
+    var curState = widgetReference.getProperty('State')
+  
+    if (String(curState) == "true") {
+      checkboxElement.prop('checked', true)
+      widgetReference.setProperty('State', true)
+    } else {
+      checkboxElement.prop('checked', false)
+      widgetReference.setProperty('State', false)
+    }
+
+    if (String(widgetReference.getProperty('Visible')) == "true") {
+      widgetElement.show()
+    } else {
+      widgetElement.hide()
+    }
+
+    if (String(widgetReference.getProperty('Disabled')) == "true") {
+      checkboxElement.prop('disabled', true)
+    } else {
+      checkboxElement.prop('enabled', false)
+    }
+
+    checkboxElement.val(widgetReference.getProperty('Value'))
+  }
+
+  this.updateProperty = function(updatePropertyInfo) {
+
+    var setProperty = function (propertyInfo, property, state) {
+      if (String(propertyInfo.SinglePropertyValue) === 'true') {
+        this.jqElement.find('input').prop(property, true)
+        this.setProperty(state, true)
       } else {
-         checkboxElement.prop('checked', false);
-         widgetReference.setProperty('State', false);
+        this.jqElement.find('input').prop(property, false)
+        this.setProperty(state, false)
       }
+    }
 
-      if (String(widgetReference.getProperty('Visible')) == "true") {
-         widgetElement.show();
+    var setVisibility = function (condition) {
+      if (String(condition) === 'true') {
+        this.jqElement.show()
+        this.setProperty('Visible', true)
       } else {
-         widgetElement.hide()
+        this.jqElement.hide()
+        this.setProperty('Visible', false)
       }
+    }
 
-      if (String(widgetReference.getProperty('Disabled')) == "true") {
-         checkboxElement.prop('disabled', true);
-      } else {
-         checkboxElement.prop('enabled', false);
-      }
+    switch (updatePropertyInfo.TargetProperty) {
+      case 'Top':
+      case 'Left':
+      case 'Width':
+      case 'Height':
+        this.setProperty(updatePropertyInfo.TargetProperty, updatePropertyInfo.SinglePropertyValue)
+        this.jqElement.css(updatePropertyInfo.TargetProperty, updatePropertyInfo.SinglePropertyValue + "px")
+      case 'State':
+        setProperty(updatePropertyInfo, 'checked', 'State')
+        break
+      case 'Disabled':
+        setProperty(updatePropertyInfo, 'disabled', 'Disabled')
+        break
+      case 'Visible':
+        setVisibility(updatePropertyInfo.SinglePropertyValue)
+        break
+      case 'Value':
+        this.jqElement.find('input').val(updatePropertyInfo.SinglePropertyValue)
+        this.setProperty('Value', updatePropertyInfo.SinglePropertyValue)
+        break
+      default:
+    }
+  }
 
-      checkboxElement.val(widgetReference.getProperty('Value'));
+  this.resetInputToDefault = function() {
+    var checkboxElement = thisWidget.jqElement.find('input')
 
-   };
+    // refactor
+    if (defaultState === true) {
+      checkboxElement.prop('checked', true)
+      thisWidget.setProperty('State', true)
+    } else {
+      checkboxElement.prop('checked', false)
+      thisWidget.setProperty('State', false)
+    }
 
-   this.updateProperty = function(updatePropertyInfo) {
+    setVisibility(defaultVisible === true)
+    
+    // refactor
+    if (defaultDisabled === true) {
+      this.jqElement.find('input').prop('disabled', true)
+      this.setProperty('Disabled', true)
+    } else {
+      this.jqElement.find('input').prop('disabled', false)
+      this.setProperty('Disabled', false)
+    }
 
-      if (updatePropertyInfo.TargetProperty === "Top" || updatePropertyInfo.TargetProperty === "Left" || updatePropertyInfo.TargetProperty === "Width" || updatePropertyInfo.TargetProperty === "Height") {
+    this.jqElement.find('input').val(defaultValue === undefined ? '' : defaultValue)
+    this.setProperty('Value', defaultValue)
+  }
 
-         this.setProperty(updatePropertyInfo.TargetProperty, updatePropertyInfo.SinglePropertyValue);
-         this.jqElement.css(updatePropertyInfo.TargetProperty, updatePropertyInfo.SinglePropertyValue + "px");
-
-      } else if (updatePropertyInfo.TargetProperty === "State") {
-
-         if (String(updatePropertyInfo.SinglePropertyValue) == "true") {
-            this.jqElement.find('input').prop('checked', true);
-            this.setProperty('State', true);
-         } else {
-            this.jqElement.find('input').prop('checked', false);
-            this.setProperty('State', false);
-         }
-
-      } else if (updatePropertyInfo.TargetProperty === "Disabled") {
-
-         if (String(updatePropertyInfo.SinglePropertyValue) == "true") {
-            this.jqElement.find('input').prop('disabled', true);
-            this.setProperty('Disabled', true);
-         } else {
-            this.jqElement.find('input').prop('disabled', false);
-            this.setProperty('Disabled', false);
-         }
-
-      } else if (updatePropertyInfo.TargetProperty === "Visible") {
-
-         if (String(updatePropertyInfo.SinglePropertyValue) == "true") {
-            this.jqElement.show();
-            this.setProperty('Visible', true);
-         } else {
-            this.jqElement.hide();
-            this.setProperty('Visible', false);
-         }
-
-      } else if (updatePropertyInfo.TargetProperty === "Value") {
-
-         this.jqElement.find('input').val(updatePropertyInfo.SinglePropertyValue);
-         this.setProperty('Value', updatePropertyInfo.SinglePropertyValue);
-
-      }
-   };
-
-   this.resetInputToDefault = function() {
-      var checkboxElement = thisWidget.jqElement.find('input');
-
-      if (defaultState === true) {
-         checkboxElement.prop('checked', true);
-         thisWidget.setProperty('State', true);
-      } else {
-         checkboxElement.prop('checked', false);
-         thisWidget.setProperty('State', false);
-      }
-
-      if (defaultVisible === true) {
-         this.jqElement.show();
-         this.setProperty('Visible', true);
-      } else {
-         this.jqElement.hide();
-         this.setProperty('Visible', false);
-      }
-
-      if (defaultDisabled === true) {
-         this.jqElement.find('input').prop('disabled', true);
-         this.setProperty('Disabled', true);
-      } else {
-         this.jqElement.find('input').prop('disabled', false);
-         this.setProperty('Disabled', false);
-      }
-
-      this.jqElement.find('input').val(defaultValue === undefined ? '' : defaultValue);
-      this.setProperty('Value', defaultValue);
-   };
-
-   this.beforeDestroy = function() {
-      try {
-         var checkboxElement = this.jqElement.find('input');
-         checkboxElement.unbind();
-      } catch (err) {
-         TW.log.error('Error in TW.Runtime.Widgets.checkbox.beforeDestroy', err);
-      }
-   };
-};
+  this.beforeDestroy = function() {
+    try {
+      var checkboxElement = this.jqElement.find('input')
+      checkboxElement.unbind()
+    } catch (err) {
+      TW.log.error('Error in TW.Runtime.Widgets.checkbox.beforeDestroy', err)
+    }
+  }
+}
